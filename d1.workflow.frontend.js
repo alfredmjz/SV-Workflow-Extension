5303df7656cc1ebcb7a37589fb1c099978de9458
alfredmjz <alfredmjz@gmail.com>
2021-12-20 14:35:22 -0500

    Update on mapFromDropdown func

--- d1.workflow.frontend.js
+++ d1.workflow.frontend.js
@@ -1,40 +1,46 @@
 (function(jq$){
-    jq$(document).ready(function(){
-        //Array of Program Office
-        var program_office_selector = jq$('[name="programOfficeId"] option');
-        var program_office = mapFromDropdown(program_office_selector);
+    'use strict';
 
-        //Convert String type to number
-        var selected_option = parseInt(jq$('[name="programOfficeId"] option:selected').val());
-        const no_cu = [5, 4, 1];
+    //Array of Program Office
+    var program_office_selector = jq$('[name="programOfficeId"] option');
+    var program_office = mapFromDropdown(program_office_selector);
 
-        //True if current Program Office have a costing unit
-        //Indexes of non costing units are: [5,4,1]
-        if(jq$.inArray(selected_option, no_cu) === -1){
-            var cu_selector = jq$('[name="costingUnitId"] option');
-            var cu_map = mapFromDropdown(cu_selector);
-        }
+    //Convert String type to number
+    var selected_option = parseInt(jq$('[name="programOfficeId"] option:selected').val());
+    const no_cu = [5, 4, 1];
 
-        //names holds two properties: assignee and wf_owner
-        var names = mapAssignee(selected_option);
+    //True if current Program Office have a costing unit
+    //Indexes of non costing units are: [5,4,1]
+    if(jq$.inArray(selected_option, no_cu) === -1){
+        var cu_selector = jq$('[name="costingUnitId"] option');
+        var cu_map = mapFromDropdown(cu_selector);
+    }
 
-        console.log(program_office);
-        console.log(cu_map);
-        console.log(names.assignee);
-        console.log(names.wf_owner);
+    //names holds two properties: assignee and wf_owner
+    var names = mapAssignee(selected_option);
 
+    jq$('[name="programOfficeId"]').closest('tr').append('<td id="flexcontainer"> <div id="flexbox">'
+    + '<div class="user-flexchild"> </div>'
+    + '<div class="event-flexchild"> </div>'
+    + '</div></td>');
 
+    jq$('.user-flexchild').append(
+        '<ul id="user-flexchild">'
+        + '<li class="flex-col-attr">User</li>'
+        + '</ul>');
 
+    jq$('.event-flexchild').append(
+        '<ul id="event-flexchild">'
+        + '<li class="flex-col-attr">Assignee</li>'
+        + '</ul>');
+    console.log(names.assignee);
 
-
-
-
-
-
-
-
+    jq$.each(names.assignee, function(index, element){
+        jq$('.user-flexchild').append('<li>' + element + '</li>');
+    })
 
         //Functions
+
         function mapAssignee(program_office){
             var assignee_selector = null;
             var wf_owner_selector = null;
@@ -85,13 +91,14 @@
             return {"assignee": assignee, "wf_owner" : wf_owner};
         }
 
-
         function mapFromDropdown(dropdown_selector){
-            var result =  new Map();
-            jq$.map(dropdown_selector, function(option){
-                result.set(option.value, option.text);
-            });
+            var result = {};
+
+            jq$(dropdown_selector).each(function(){
+                result[parseInt(this.value)] = this.text;
+            })
+
             return result;
         }
-    })
+
 })(jQuery);
